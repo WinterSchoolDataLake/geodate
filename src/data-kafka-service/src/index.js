8ea6451890e6d6ab28e009a658b41ce526356136
Vyacheslav-M <asgeneral96@gmail.com>
2020-02-02 17:04:35 +0700

    add data-generate container network host

--- src/data-kafka-service/src/index.js
+++ src/data-kafka-service/src/index.js
@@ -2,7 +2,9 @@ const kafka = require('kafka-node')
 const faker = require('faker')
 
 // CONSTANTS
-const TOPIC_NAME = "geodata"
+const TOPIC_NAME = "geodata-1"
+const KAFKA_HOST = process.env.KAFKA_CLIENT_CONNECT
+console.log(KAFKA_HOST);
 const DEFAULT_TOPIC = { topic: TOPIC_NAME, partitions: 1, replicationFactor: 1}
 
 // CORE
@@ -17,12 +19,14 @@ function createTopics() {
 
 // CONSUMER
 function setupConsumer() {
+    console.log('setupConsumer');
     const consumer = new Consumer(Client, [DEFAULT_TOPIC], {})
     consumer.on('message', message => console.log(`message ${message.value} was received`))
 }
 
 // PRODUCER
 function startProducer() {
+    console.log('startProducer');
     const producer = new Producer(Client)
 
     const getRandomRecord = () => ({
@@ -32,11 +36,12 @@ function startProducer() {
     })
 
     producer.on('ready', _ => {
+        console.log('Producer ready');
         const sendFn = data => producer.send([{
                 topic: TOPIC_NAME,
                 messages: [JSON.stringify(data)],
             }],
-            () => console.log(`${data.id} had sent`))
+            () => console.log(`${JSON.stringify(data)} had sent`))
         setInterval(() => sendFn(getRandomRecord()), 1500)
     })
 }
